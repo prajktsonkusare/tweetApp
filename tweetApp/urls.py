5657094f98ffd00f44cc512bcd43ea486c57a1c7
Prajkt Sonkusare <137401330+prajktsonkusare@users.noreply.github.com>
2024-08-03 01:10:34 +0530

    urls.py

--- tweetApp/urls.py
+++ tweetApp/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path("tweet/", include('tweet.urls')),
+    path("tweet/", include('tweet.urls')),
     path('', include('tweet.urls')),#me
     path("accounts/", include('django.contrib.auth.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
