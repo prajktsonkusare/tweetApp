c76c0fac546c9c1b8a71cef141145988e46691b7
Prajkt Sonkusare <137401330+prajktsonkusare@users.noreply.github.com>
2024-08-03 00:24:59 +0530

    settings.py

--- tweetApp/settings.py
+++ tweetApp/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = "tweetApp.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
